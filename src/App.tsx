e1a599d5da5bf8a0b10883bd5fdc48f1125eb80b
W.J. “dysbulic” H <dys@dhappy.org>
2022-09-27 19:19:08 -0400

    adding a header image & the beginnings of content 🍉

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ const App = () => {
         <object data="header.svg"/>
       </header>
       <main>
-        <p>Fat.Rocks is an experiment in creating a privacy-preserving market, specifically for illegal substances using zero-knowledge, encryption, and token-gating rather than The Onion Router.</p>
-        <p>It is the author's opinion that though it hasn't been as brutal, the War On Drugs has been regressive on par with slavery. Future generations will look back on our mass incarcerations, militarization of police, and criminalization of behavior; and wonder how normal people thought this was ok. Much like we do with slavery, or crucifixion, or whatever.</p>
+        <p>Fat.Rocks is an experiment in creating a privacy-preserving market, specifically for illegal substances, using zero-knowledge, encryption, and token-gating rather than leveraging The Onion Router as is most common today.</p>
+        <p>It is the author's opinion that though it hasn't been as brutal, the War On Drugs is societally regressive on par with slavery. Future generations will look back on our mass incarcerations, militarization of police, and criminalization of behavior; and wonder how normal people thought this was ok. Much like most of us do with slavery, or crucifixion, or whatever other brutality we've outgrown.</p>
+        <p>The project is beginning with a token-gated product catalog. Images of substances for sale are photographed as they are packaged and customers can peruse the listings they have access to.</p>
+        <p>Far example, … <em>(To be continued shortly…)</em></p>
       </main>
     </>
   )
